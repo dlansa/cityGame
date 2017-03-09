450b0f52a4fcb2864ff553e824f82822811de59f
Shaktulo Veronika <shkatulon@gmail.com>
2017-03-09 21:00:32 +0300

    INITIALIZATION OF THIS BEST EVER CODE ^__^

---
+++ src/by/htp/entity/City.java
@@ -0,0 +1,42 @@
+package by.htp.entity;
+
+public class City {
+
+    private String name;
+
+    public City() {
+    }
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City)) return false;
+
+        City city = (City) o;
+
+        return getName() != null ? getName().equals(city.getName()) : city.getName() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return getName() != null ? getName().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
